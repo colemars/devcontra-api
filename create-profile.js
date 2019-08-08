e712f9e3428be8b71f317702790d67ed637d558f
Cole Marsteller <cgm32547@gmail.com>
2019-08-07 22:23:35 -0700

    add logs for testing

--- create-profile.js
+++ create-profile.js
@@ -3,25 +3,26 @@ import { success, failure } from "./libs/response-lib";
 
 export default async function main(event) {
   const data = JSON.parse(event.body);
-
+  console.log(process.env.tableName);
+  console.log(process.env.bucketName);
   // const images = await snapshot(data.siteName, data.siteUserName);
   // const imageKeys = await uploadToBucket(images);
 
-  const params = {
-    TableName: process.env.tableName,
-    Item: {
-      userId: data.userId,
-      siteName: data.siteName,
-      // imageKeys,
-      createdAt: Date.now()
-    }
-  };
+  // const params = {
+  //   TableName: process.env.tableName,
+  //   Item: {
+  //     userId: data.userId,
+  //     siteName: data.siteName,
+  //     // imageKeys,
+  //     createdAt: Date.now()
+  //   }
+  // };
 
-  try {
-    const result = await dynamoDbLib.call("put", params);
-    return success(result);
-  } catch (e) {
-    console.log(e);
-    return failure({ status: false });
-  }
+  // try {
+  //   const result = await dynamoDbLib.call("put", params);
+  //   return success(result);
+  // } catch (e) {
+  //   console.log(e);
+  //   return failure({ status: false });
+  // }
 }
