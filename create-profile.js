3b74d23d544a4011bc16f1c835a27f68090d6476
Cole Marsteller <cgm32547@gmail.com>
2019-08-07 21:46:17 -0700

    add create-profile lambda fn

---
+++ create-profile.js
@@ -0,0 +1,27 @@
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+export default async function main(event) {
+  const data = JSON.parse(event.body);
+
+  // const images = await snapshot(data.siteName, data.siteUserName);
+  // const imageKeys = await uploadToBucket(images);
+
+  const params = {
+    TableName: process.env.tableName,
+    Item: {
+      userId: data.userId,
+      siteName: data.siteName,
+      // imageKeys,
+      createdAt: Date.now()
+    }
+  };
+
+  try {
+    const result = await dynamoDbLib.call("put", params);
+    return success(result);
+  } catch (e) {
+    console.log(e);
+    return failure({ status: false });
+  }
+}
