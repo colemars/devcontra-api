6c4c79eef0158b9c596e425c0f52757c273e2774
Cole Marsteller <cgm32547@gmail.com>
2019-08-08 20:01:17 -0700

    fix typo and remove logs

--- create-profile.js
+++ create-profile.js
@@ -5,21 +5,18 @@ import { success, failure } from "./libs/response-lib";
 
 export default async function main(event) {
   const data = JSON.parse(event.body);
-  console.log(data);
-  const siteName = "stackOverflow";
-  const accountUrl =
-    "https://stackoverflow.com/users/10606984/colemars?tab=questions";
-  const site = data.siteName.toString().toLowerCase();
-  const url = data.url.toString().toLowercase();
+  const { userId } = data;
+  const siteName = data.siteName.toLowerCase();
+  const url = data.url.toLowerCase();
 
-  await snapshot(site, url);
+  await snapshot(siteName, url);
   const keys = await s3Upload();
 
   const params = {
     TableName: process.env.tableName,
     Item: {
-      userId: data.userId,
-      siteName: data.siteName,
+      userId,
+      siteName,
       imageKeys: keys,
       createdAt: Date.now()
     }
