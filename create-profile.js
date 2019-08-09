d6ba8266255d195fa8b1f36a3f10650d20a88d43
Cole Marsteller <cgm32547@gmail.com>
2019-08-08 22:48:47 -0700

    test refactored snapshot

--- create-profile.js
+++ create-profile.js
@@ -2,7 +2,9 @@ import fs from "fs";
 import util from "util";
 import * as dynamoDbLib from "./libs/dynamodb-lib";
 import s3Upload from "./helpers/upload-to-bucket";
-import snapshot from "./helpers/snapshot";
+// import snapshot from "./helpers/snapshot";
+import altSnapshot from "./helpers/alt-snapshot";
+import fetchUrls from "./helpers/fetch-urls";
 import { success, failure } from "./libs/response-lib";
 
 const mkdir = util.promisify(fs.mkdir);
@@ -10,43 +12,49 @@ const fsStat = util.promisify(fs.stat);
 const fsRmDir = util.promisify(fs.rmdir);
 
 export default async function main(event) {
-  const data = JSON.parse(event.body);
-  const { userId } = data;
-  const siteName = data.siteName.toLowerCase();
-  const url = data.url.toLowerCase();
+  // const data = JSON.parse(event.body);
+  // const { userId } = data;
+  // const siteName = data.siteName.toLowerCase();
+  // const url = data.url.toLowerCase();
 
-  try {
-    await fsStat("/tmp/img");
-    console.log("exists");
-  } catch (e) {
-    console.log("creating director");
-    await mkdir("/tmp/img");
-  }
+  // try {
+  //   await fsStat("/tmp/img");
+  //   console.log("exists");
+  // } catch (e) {
+  //   console.log("creating director");
+  //   await mkdir("/tmp/img");
+  // }
 
-  await snapshot(siteName, url);
-  const keys = await s3Upload();
+  // await snapshot(siteName, url);
 
-  try {
-    await fsRmDir("/tmp/img");
-  } catch (e) {
-    console.log(e);
-  }
+  const urls = await fetchUrls();
+  await altSnapshot(urls);
 
-  const params = {
-    TableName: process.env.tableName,
-    Item: {
-      userId,
-      siteName,
-      imageKeys: keys,
-      createdAt: Date.now()
-    }
-  };
+  // const keys = await s3Upload();
 
-  try {
-    const result = await dynamoDbLib.call("put", params);
-    return success(result);
-  } catch (e) {
-    console.log(e);
-    return failure({ status: false });
-  }
+  // console.log("out! Keys:", keys);
+
+  // try {
+  //   await fsRmDir("/tmp/img");
+  // } catch (e) {
+  //   console.log(e);
+  // }
+
+  // const params = {
+  //   TableName: process.env.tableName,
+  //   Item: {
+  //     userId,
+  //     siteName,
+  //     imageKeys: keys,
+  //     createdAt: Date.now()
+  //   }
+  // };
+
+  // try {
+  //   const result = await dynamoDbLib.call("put", params);
+  //   return success(result);
+  // } catch (e) {
+  //   console.log(e);
+  //   return failure({ status: false });
+  // }
 }
