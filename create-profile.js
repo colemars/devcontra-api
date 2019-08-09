330bfc38b24c4c1522f1b92df78f61fa820d1c48
Cole Marsteller <cgm32547@gmail.com>
2019-08-08 21:03:12 -0700

    try to mkdir and then delete it when it's empty

--- create-profile.js
+++ create-profile.js
@@ -1,7 +1,13 @@
+import fs from "fs";
+import util from "util";
 import * as dynamoDbLib from "./libs/dynamodb-lib";
 import s3Upload from "./helpers/upload-to-bucket";
 import snapshot from "./helpers/snapshot";
 import { success, failure } from "./libs/response-lib";
+
+const mkdir = util.promisify(fs.mkdir);
+const fsStat = util.promisify(fs.stat);
+const fsRmDir = util.promisify(fs.rmdir);
 
 export default async function main(event) {
   const data = JSON.parse(event.body);
@@ -9,8 +15,22 @@ export default async function main(event) {
   const siteName = data.siteName.toLowerCase();
   const url = data.url.toLowerCase();
 
+  try {
+    await fsStat("/tmp/img");
+    console.log("exists");
+  } catch (e) {
+    console.log("creating director");
+    await mkdir("/tmp/img");
+  }
+
   await snapshot(siteName, url);
   const keys = await s3Upload();
+
+  try {
+    await fsRmDir("/tmp/img");
+  } catch (e) {
+    console.log(e);
+  }
 
   const params = {
     TableName: process.env.tableName,
