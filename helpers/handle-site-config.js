b57addafb792c553ea4efe342d0b9e373c4ffa05
Cole Marsteller <cgm32547@gmail.com>
2019-08-09 00:20:34 -0700

    fix nesting error

--- helpers/handle-site-config.js
+++ helpers/handle-site-config.js
@@ -1,15 +1,13 @@
 const handleSiteConfig = async (url, siteName) => {
-  if (!url.includes("https")) return { urlProps: { error: "not a valid url" } };
+  if (!url.includes("https")) return { error: "not a valid url" };
   if (siteName === "stackoverflow")
     return {
-      urlProps: {
-        root: "https://stackoverflow.com",
-        urlParams: ["?tab=answers", "?tab=questions"],
-        selectors: [".question-hyperlink", ".answer-hyperlink"]
-      }
+      root: "https://stackoverflow.com",
+      urlParams: ["?tab=answers", "?tab=questions"],
+      selectors: [".question-hyperlink", ".answer-hyperlink"]
     };
 
-  return { urlProps: { error: "not a valid site" } };
+  return { error: "not a valid site" };
 };
 
 export default handleSiteConfig;
