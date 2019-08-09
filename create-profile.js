47e474fc8df36e672f07c13247ffe4aefd0ab7c5
Cole Marsteller <cgm32547@gmail.com>
2019-08-08 19:20:19 -0700

    clean up code

--- create-profile.js
+++ create-profile.js
@@ -4,33 +4,33 @@ import snapshot from "./helpers/snapshot";
 import { success, failure } from "./libs/response-lib";
 
 export default async function main(event) {
-  console.log("run");
-  // const data = JSON.parse(event.body);
-  const siteName = "stackOverflow";
-  const accountUrl = "https://stackoverflow.com/users/10606984/colemars?tab=questions";
-  const images = await snapshot(siteName, accountUrl);
-  console.log("out of snapshot");
-  console.log("images", images);
-  const imageKeys = await s3Upload(images);
-  console.log("out of Upload");
-  console.log("imageKeys", imageKeys);
-  // const imageKeys = await uploadToBucket(images);
+  const data = JSON.parse(event.body);
+  // const siteName = "stackOverflow";
+  // const accountUrl =
+  //   "https://stackoverflow.com/users/10606984/colemars?tab=questions";
+  const site = data.siteName.toLowerCase();
+  const url = data.url.toLowercase();
 
-  // const params = {
-  //   TableName: process.env.tableName,
-  //   Item: {
-  //     userId: data.userId,
-  //     siteName: data.siteName,
-  //     // imageKeys,
-  //     createdAt: Date.now()
-  //   }
-  // };
+  await snapshot(site, url);
+  const keys = await s3Upload();
 
-  // try {
-  //   const result = await dynamoDbLib.call("put", params);
-  //   return success(result);
-  // } catch (e) {
-  //   console.log(e);
-  //   return failure({ status: false });
-  // }
+  const params = {
+    TableName: process.env.tableName,
+    Item: {
+      userId: data.userId,
+      siteName: data.siteName,
+      imageKeys: keys,
+      createdAt: Date.now()
+    }
+  };
+
+  try {
+    const result = await dynamoDbLib.call("put", params);
+    return success(result);
+  } catch (e) {
+    console.log(e);
+    return failure({ status: false });
+  }
 }
+
+main();
