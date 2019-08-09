aef94b7fa340482e3611e55eeddaad07828ade1b
Cole Marsteller <cgm32547@gmail.com>
2019-08-09 02:24:34 -0700

    revert to fn call

--- create-profile.js
+++ create-profile.js
@@ -1,5 +1,3 @@
-import fs from "fs";
-import util from "util";
 import * as dynamoDbLib from "./libs/dynamodb-lib";
 import * as handleTmpDir from "./helpers/handle-tmp-image-dir";
 import uploadToBucket from "./helpers/upload-to-bucket";
@@ -7,10 +5,6 @@ import snapshot from "./helpers/snapshot";
 import fetchUrls from "./helpers/fetch-urls";
 import handleSiteConfig from "./helpers/handle-site-config";
 import { success, failure } from "./libs/response-lib";
-
-const mkdir = util.promisify(fs.mkdir);
-const fsStat = util.promisify(fs.stat);
-const fsRmDir = util.promisify(fs.rmdir);
 
 export default async function main(event) {
   const data = JSON.parse(event.body);
@@ -25,27 +19,12 @@ export default async function main(event) {
   const urls = await fetchUrls(root, urlParams, selectors, url);
   console.log("done with fetch");
 
-  try {
-    console.log("checking if dir exists...");
-    await fsStat("/tmp/img");
-    console.log("dir exists");
-  } catch (e) {
-    console.log("creating dir");
-    await mkdir("/tmp/img");
-  }
-
+  await handleTmpDir.create();
   await snapshot(urls);
   console.log("done with snapshot");
   const keys = await uploadToBucket();
   console.log("done with upload");
-
-  try {
-    console.log("removing temp img dir...");
-    await fsRmDir("/tmp/img");
-    console.log("dir removed");
-  } catch (e) {
-    console.log("remove failed", e);
-  }
+  await handleTmpDir.remove();
 
   // const params = {
   //   TableName: process.env.tableName,
