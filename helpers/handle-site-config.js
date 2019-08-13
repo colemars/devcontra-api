b4a816c66a4d4a723c45b096c15021f3fa7c7e3c
Cole Marsteller <cgm32547@gmail.com>
2019-08-13 02:08:48 -0700

    update sitename verification

--- helpers/handle-site-config.js
+++ helpers/handle-site-config.js
@@ -1,12 +1,12 @@
 const handleSiteConfig = async (url, siteName) => {
   if (!url.includes("https")) return { error: "not a valid url" };
-  if (siteName === "stack")
+  if (siteName === "stackoverflow") {
     return {
       root: "https://stackoverflow.com",
       urlParams: ["?tab=answers", "?tab=questions"],
       selectors: [".question-hyperlink", ".answer-hyperlink"]
     };
-
+  }
   return { error: "not a valid url" };
 };
 
