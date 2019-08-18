ff08d00e50b7724ac9bb13321ac37f138eacde8c
Cole Marsteller <cgm32547@gmail.com>
2019-08-17 17:41:54 -0700

    use userPoolUserId instead of identityId as key

--- create-profile.js
+++ create-profile.js
@@ -189,17 +189,21 @@ const handleVariant = async (variant, targetUserId) => {
 };
 
 export default async function main(event) {
+  const authProvider =
+    event.requestContext.identity.cognitoAuthenticationProvider;
+  const parts = authProvider.split(":");
+  const userPoolIdParts = parts[parts.length - 3].split("/");
+  const userPoolId = userPoolIdParts[userPoolIdParts.length - 1];
+  const userPoolUserId = parts[parts.length - 1];
   const data = JSON.parse(event.body);
   const userId = event.requestContext.identity.cognitoIdentityId;
   const { variant, targetUserId } = data;
-
-  console.log(targetUserId, variant);
 
   const { response, error } = await handleVariant(variant, targetUserId);
   if (error) return failure(error);
 
     const upload = await Promise.all(
-      response.map(result => dynamoDbUpload(result, userId, variant))
+    response.map(result => dynamoDbUpload(result, userPoolUserId, variant))
     );
   if (!upload.every(item => item === true))
     // TO DO build error parser;
