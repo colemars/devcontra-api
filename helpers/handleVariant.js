e160f1d89d1f824d16f33fabde71d43a8e64c92a
Cole Marsteller <cgm32547@gmail.com>
2019-09-14 04:13:00 -0700

    handle github

--- helpers/handleVariant.js
+++ helpers/handleVariant.js
@@ -1,5 +1,15 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable camelcase */
 import fetch from "node-fetch";
 import moment from "moment";
+
+const constructReturnObject = (id, date, type) => {
+  return {
+    id,
+    date,
+    type
+  };
+};
 
 const handleStackOverflow = async targetUserId => {
   const now = moment().utc();
@@ -22,27 +32,82 @@ const handleStackOverflow = async targetUserId => {
     jsonify(await fetch(getActivity))
   ]);
 
-  const activity = activityData.items;
   if (!activityData) return { error: "Invalid user id" };
+  const activity = activityData.items;
   if (activity.length === 0)
     return { error: "This user has no posts to fetch" };
 
-  activity.map(item => {
-    const creationDate = moment.unix(item.creation_date);
+  const returnObject = activity.map(item => {
+    const { creation_date, timeline_type } = item;
+    const creationDate = moment.unix(creation_date);
     const begOfWeek = creationDate
       .clone()
       .startOf("week")
       .unix();
-    // eslint-disable-next-line no-param-reassign
-    item.activityDate = begOfWeek;
-    return item;
+    return constructReturnObject(creation_date, begOfWeek, timeline_type);
   });
-  return { response: activity };
+  return { response: returnObject };
 };
 
 const handleSpectrum = targetUserId => {};
 const handleTwitter = targetUserId => {};
-const handleGithub = targetUserId => {};
+const handleGithub = async targetUserId => {
+  const now = moment().utc();
+  const fromDate = now
+    .clone()
+    .subtract(17, "weeks")
+    .startOf("week")
+    .format("YYYY-MM-DD");
+
+  const getCommitActivity = `https://api.github.com/search/commits?q=author:${targetUserId}+committer-date:>=${fromDate}`;
+  const getPRActivity = `https://api.github.com/search/issues?q=author:${targetUserId}+type:pr+created:>=${fromDate}`;
+  const getIssueActivity = `https://api.github.com/search/issues?q=author:${targetUserId}+type:issue+created:>=${fromDate}`;
+
+  const jsonify = async target => {
+    const result = await target.json();
+    return result.items;
+  };
+
+  const options = {
+    headers: { Accept: "application/vnd.github.cloak-preview" }
+  };
+
+  const [commits, issues, pullRequests] = await Promise.all([
+    jsonify(await fetch(getCommitActivity, options)),
+    jsonify(await fetch(getIssueActivity, options)),
+    jsonify(await fetch(getPRActivity, options))
+  ]);
+
+  commits.map(type => {
+    type.type = "commit";
+    type.created_at = type.commit.committer.date;
+    return type;
+  });
+
+  issues.map(type => {
+    type.type = "issue";
+    return type;
+  });
+
+  pullRequests.map(type => {
+    type.type = "pr";
+    return type;
+  });
+
+  const activity = [].concat(...[commits, issues, pullRequests]);
+
+  const returnObject = activity.map(item => {
+    const { created_at, type } = item;
+    const creationDate = moment(created_at);
+    const begOfWeek = creationDate
+      .clone()
+      .startOf("week")
+      .unix();
+    item.activityDate = begOfWeek;
+    return constructReturnObject(creationDate.unix(), begOfWeek, type);
+  });
+  return { response: returnObject };
+};
 
 const handleVariant = async (variant, targetUserId) => {
   if (variant === "stackoverflow") return handleStackOverflow(targetUserId);
