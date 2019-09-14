660f8bbbaaa1bfe8c0a8c30bcb570352beb9ea14
Cole Marsteller <cgm32547@gmail.com>
2019-09-14 04:12:31 -0700

    update db structure

--- helpers/dynamoDbUpload.js
+++ helpers/dynamoDbUpload.js
@@ -2,32 +2,16 @@
 import * as dynamoDbLib from "../libs/dynamodb-lib";
 
 const dynamoDbUpload = async (result, userId, variant, profileUrl) => {
-  const {
-    timeline_type,
-    creation_date,
-    detail,
-    title,
-    post_type,
-    comment_id,
-    post_id,
-    activityDate
-  } = result;
-
-  const nonPost = 100;
+  const { id, date, type } = result;
 
   const params = {
     TableName: process.env.tableName,
     Item: {
       userId,
-      timelineType: timeline_type,
+      activityId: id,
       variant,
-      activityDate,
-      title,
-      detail,
-      postType: post_type,
-      commentId: comment_id,
-      postId: creation_date,
-      activityId: post_id || nonPost,
+      activityDate: date,
+      type,
       profileUrl,
       createdAt: Date.now()
     }
