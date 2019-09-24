082ad0c46a4bb41241481f5f4f742cc5080b78c2
Cole Marsteller <cgm32547@gmail.com>
2019-09-24 00:48:38 -0700

    naming

--- populate-tags.js
+++ populate-tags.js
@@ -1,12 +1,12 @@
 import { success, failure } from "./libs/response-lib";
 import handleStackTags from "./helpers/handleStackTags";
-import dynamoDbStackTagsUpload from "./helpers/dynamoDbStackTagsUpload";
+import s3StackTagsUpload from "./helpers/s3StackTagsUpload";
 
 export default async function main() {
   const { result, error } = await handleStackTags();
   if (error) return failure(error);
   try {
-    await dynamoDbStackTagsUpload(result);
+    await s3StackTagsUpload(result);
     return success(`tags populated`);
   } catch (err) {
     return failure(err);
