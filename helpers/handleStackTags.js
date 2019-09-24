35e502be7566a519a348a3b198522cafb9a247c7
Cole Marsteller <cgm32547@gmail.com>
2019-09-24 00:10:55 -0700

    fix handling response data

--- helpers/handleStackTags.js
+++ helpers/handleStackTags.js
@@ -7,7 +7,6 @@ const jsonify = async target => {
 };
 
 const buildTags = async (page, tags) => {
-  console.log(page);
   const getTags = `https://api.stackexchange.com/2.2/tags?page=${page}&pagesize=100&order=desc&sort=popular&site=stackoverflow`;
   const { items, error_name, quota_remaining, has_more } = await jsonify(
     await fetch(getTags)
@@ -16,12 +15,14 @@ const buildTags = async (page, tags) => {
   tags.push(items);
   if (quota_remaining === 0) return { result: tags };
   if (has_more) return buildTags(page + 1, tags);
-  return { result: tags };
+  const flattenedTags = [].concat.apply([], [...tags]);
+  const surfacedTags = flattenedTags.map(item => item.name);
+
+  return { result: surfacedTags };
 };
 
 const handleStackTags = async () => {
   const { result, error } = await buildTags(1, []);
-  console.log(result, error);
   if (error) return { error };
   return { result };
 };
