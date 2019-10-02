b1817299c46f51de8c7ecfaf350d72299937156d
Cole Marsteller <cgm32547@gmail.com>
2019-10-02 11:27:02 -0700

    add method for updating profile access key

---
+++ update-profile-key.js
@@ -0,0 +1,28 @@
+import { v4 } from "uuid";
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+export default async function main(event) {
+  const authProvider =
+    event.requestContext.identity.cognitoAuthenticationProvider;
+  const parts = authProvider.split(":");
+  const userPoolUserId = parts[parts.length - 1];
+  const params = {
+    TableName: process.env.tableName,
+    Key: {
+      userId: userPoolUserId
+    },
+    UpdateExpression: "SET accessKey = :accessKey",
+    ExpressionAttributeValues: {
+      ":accessKey": v4()
+    },
+    ReturnValues: "UPDATED_NEW"
+  };
+
+  try {
+    const result = await dynamoDbLib.call("update", params);
+    return success(result);
+  } catch (e) {
+    return failure({ status: false });
+  }
+}
