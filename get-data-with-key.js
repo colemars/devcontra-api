a1dd3d73ca6b04a88da95b9f8ac97b58e54e1fe3
Cole Marsteller <cgm32547@gmail.com>
2019-10-03 16:49:19 -0700

    get userId from accessKey

--- get-data-with-key.js
+++ get-data-with-key.js
@@ -1,25 +1,48 @@
+/* eslint-disable prefer-destructuring */
 import * as dynamoDbLib from "./libs/dynamodb-lib";
 import { success, failure } from "./libs/response-lib";
 
 export default async function main(event) {
-  console.log("hit");
   const data = JSON.parse(event.body);
   const { variant } = event.pathParameters;
   const { accessKey } = data;
+  let userId;
 
-  const params = {
-    TableName: process.env.tableName,
-    IndexName: "variant-accessKey-index",
-    KeyConditionExpression: "variant = :variant and accessKey = :accessKey",
+  const userParams = {
+    TableName: process.env.usersTableName,
+    IndexName: "accessKey-index",
+    KeyConditionExpression: "accessKey = :accessKey",
     ExpressionAttributeValues: {
-      ":variant": variant,
       ":accessKey": accessKey
+    },
+    AttributesToGet: "userId",
+    Select: "SPECIFIC_ATTRIBUTES"
+  };
+
+  try {
+    console.log("here");
+    const userResult = await dynamoDbLib.call("query", userParams);
+    console.log(userResult);
+    userId = userResult.Items[0].userId;
+  } catch (e) {
+    console.log("error:", e);
+    return failure({ error: e, status: false });
+  }
+
+  const profileParams = {
+    TableName: process.env.profilesTableName,
+    IndexName: "userId-variant-index",
+    KeyConditionExpression: "userId = :id and variant = :variant",
+    ExpressionAttributeValues: {
+      ":id": userId,
+      ":variant": variant
     },
     Select: "ALL_PROJECTED_ATTRIBUTES"
   };
 
   try {
-    const profile = await dynamoDbLib.call("query", params);
+    console.log("there");
+    const profile = await dynamoDbLib.call("query", profileParams);
     console.log(profile);
     if (profile.Items) {
       return success(profile.Items);
