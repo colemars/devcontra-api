8d6528758516669621aabbd0acfd5d433247cd9d
Cole Marsteller <cgm32547@gmail.com>
2019-10-08 11:15:12 -0700

    update method names

--- get-profile-key.js
+++ get-profile-key.js
@@ -1,4 +1,3 @@
-import { v4 } from "uuid";
 import * as dynamoDbLib from "./libs/dynamodb-lib";
 import { success, failure } from "./libs/response-lib";
 
@@ -8,22 +7,15 @@ export default async function main(event) {
   const parts = authProvider.split(":");
   const userPoolUserId = parts[parts.length - 1];
 
-  const newKey = v4();
-
   const params = {
     TableName: process.env.usersTableName,
     Key: {
       userId: userPoolUserId
-    },
-    UpdateExpression: "SET accessKey = :accessKey",
-    ExpressionAttributeValues: {
-      ":accessKey": newKey
-    },
-    ReturnValues: "UPDATED_NEW"
+    }
   };
 
   try {
-    const result = await dynamoDbLib.call("update", params);
+    const result = await dynamoDbLib.call("getItem", params);
     console.log(result);
     return success(result);
   } catch (e) {
