64527fdd28cbb3064cdc39af26ed48057031f6f9
Cole Marsteller <cgm32547@gmail.com>
2019-10-07 21:12:02 -0700

    add get method for profile key

---
+++ get-profile-key.js
@@ -0,0 +1,25 @@
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+export default async function main(event) {
+  const authProvider =
+    event.requestContext.identity.cognitoAuthenticationProvider;
+  const parts = authProvider.split(":");
+  const userPoolUserId = parts[parts.length - 1];
+
+  const params = {
+    TableName: process.env.usersTableName,
+    Key: {
+      userId: userPoolUserId
+    }
+  };
+
+  try {
+    const result = await dynamoDbLib.call("getItem", params);
+    console.log(result);
+    return success(result);
+  } catch (e) {
+    console.log(e);
+    return failure({ status: false });
+  }
+}
